b2ac37578530cea0819a9d2b89f39ccdc6bf9234
Kamal Nayan <sknasylum8454@gmail.com>
2023-05-06 15:12:31 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+   
+    <nav>
+      
+      <li><span>01.</span>About</li>
+      <li><span>02.</span>Experience</li>
+      <li><span>03.</span>Work</li>
+      <li><span>04.</span>Contact</li>
+      <button className='resume'>Resume</button>
+      
+    </nav>
+
+  );
+}
+
+export default App;
